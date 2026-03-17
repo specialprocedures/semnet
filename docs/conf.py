017bc17fb2e05c20c89dc94414bf897fa85ca0d7
Ian Goodrich <goodrichian@gmail.com>
2026-03-17 23:31:56 +0000

    Bump version to 0.1.8

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ release = "0.1.0"
 copyright = "2025, Ian Goodrich"
 
 # The short X.Y version
-version = "0.1.7"
+version = "0.1.8"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
